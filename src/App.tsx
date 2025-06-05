72cb130f3e0e52711c70e8096352fd17f7497a06
siva222003 <sivapidugu02@gmail.com>
2025-06-05 21:52:54 +0530

    eslint setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <h1>HELLo</h1>
-  )
+  return <h1>HELLo</h1>;
 }
 
-export default App
+export default App;
