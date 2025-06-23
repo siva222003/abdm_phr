a21ebc1b115d059f72e4107620ca437e9cb96613
siva222003 <sivapidugu02@gmail.com>
2025-06-23 11:45:19 +0530

    added profile and password update flows

--- src/components/profile/EditProfileForm.tsx
+++ src/components/profile/EditProfileForm.tsx
@@ -135,7 +135,7 @@ export default function EditProfileForm({
           type="submit"
           className="w-full"
           variant="primary"
-          disabled={!form.formState.isDirty || updateProfileMutation.isPending}
+          disabled={isSubmitDisabled}
         >
           {updateProfileMutation.isPending ? (
             <>
